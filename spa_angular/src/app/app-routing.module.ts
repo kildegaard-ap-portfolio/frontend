d2232535b14dfa238f6d5222a38d68725f9378b2
Gustavo <gustavo.kildegaard@gmail.com>
2022-10-09 08:41:32 -0300

    agregado parte del routing

--- spa_angular/src/app/app-routing.module.ts
+++ spa_angular/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { EncabezadoComponent } from './componentes/encabezado/encabezado.component';
+import { MainComponent } from './componentes/main/main.component';
+import { FooterComponent } from './componentes/footer/footer.component';
+
+
+const routes: Routes = [
+  { path: 'Encabezado', component: EncabezadoComponent },
+  { path: 'Main', component: MainComponent },
+  { path: 'Footer', component: FooterComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
